fe377d88a462e3f4ac05f0d501df07695298ab31
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2025-07-14 19:20:54 +0300

    Delete hel-sys-manager A directory

--- hel-sys-manager A/up.sh
+++
@@ -1,8 +0,0 @@
-touch src/__init__.py
-touch src/ui/__init__.py
-touch src/core/__init__.py
-touch src/assets/__init__.py
-touch src/assets/icons/__init__.py
-touch src/assets/icons/tab_icons/__init__.py
-touch src/assets/images/__init__.py
-touch src/assets/styles/__init__.py
