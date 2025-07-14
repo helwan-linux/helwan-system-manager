65a18e3abc87873de8534c60a57077ab0c7e2e1f
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2025-07-14 12:43:42 +0300

    Add files via upload

---
+++ hel-sys-manager 2/up.sh
@@ -0,0 +1,8 @@
+touch src/__init__.py
+touch src/ui/__init__.py
+touch src/core/__init__.py
+touch src/assets/__init__.py
+touch src/assets/icons/__init__.py
+touch src/assets/icons/tab_icons/__init__.py
+touch src/assets/images/__init__.py
+touch src/assets/styles/__init__.py
